e4cb42bc549c5a86f5379fb5d929ec529585fe5f
woni <woni@edetw.com>
2025-12-16 16:05:54 +0800

    feat: 新增外掛面板

---
+++ Ede.Uofx.Customize.Web/ClientApp/src/app/web/panels/user/hello-world/customer-list/customer-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { UofxPluginPanel } from '@uofx/plugin';
+
+@Component({
+  selector: 'app-customer-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-list.component.html',
+  styleUrl: './customer-list.component.scss'
+})
+export class CustomerListComponent extends UofxPluginPanel{
+
+}
--- Ede.Uofx.Customize.Web/ClientApp/webpack-exposes.config.js
+++ Ede.Uofx.Customize.Web/ClientApp/webpack-exposes.config.js
@@ -12,6 +12,7 @@ const exposes = {
     './Panel/User': './src/app/web/panels/user/hello-world/hello-world.component.ts',
     './OrderFieldComplete': './src/app/web/order-field-complete/order-field-complete.module.ts',
     './Template': './src/app/web/template/your-field-name.module.ts',
+    './Panel/CustomerList': './src/app/web/panels/user/hello-world/customer-list/customer-list.component.ts'
   },
   app: {
     // ./Page 名稱固定，為手機端進入點
