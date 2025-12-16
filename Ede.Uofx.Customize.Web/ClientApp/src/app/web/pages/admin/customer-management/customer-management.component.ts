5d11ff56d911b3dd8a0deecf38734fee258b412b
woni <woni@edetw.com>
2025-12-16 15:25:47 +0800

    feat: 複製並修改模板資料

--- Ede.Uofx.Customize.Web/ClientApp/src/app/web/pages/admin/customer-management/customer-management.component.ts
+++ Ede.Uofx.Customize.Web/ClientApp/src/app/web/pages/admin/customer-management/customer-management.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { NorthWindService } from '@service/northwind.service';
 import { UofxPluginAuthorize, UofxPluginPage } from '@uofx/plugin';
+import { MenuItem } from 'primeng/api';
 
 @UofxPluginAuthorize({ functionId: 'CUSTOMERMANAGEMENT' })
 @Component({
@@ -7,6 +9,31 @@ import { UofxPluginAuthorize, UofxPluginPage } from '@uofx/plugin';
   templateUrl: './customer-management.component.html',
   styleUrl: './customer-management.component.scss'
 })
-export class CustomerManagementComponent extends UofxPluginPage{
+export class CustomerManagementComponent extends UofxPluginPage {
 
+  constructor(private northwindServ: NorthWindService){
+    super();
+  }
+
+  /** 麵包屑清單 */
+  breadCrumbItems: Array<MenuItem> = [
+    { label: '大廳', routerLink: [this.baseUrl.admin] },
+    { label: '客戶管理' },
+  ];
+  /** 列表清單 */
+  dataList = [];
+
+  ngOnInit() {
+    this.northwindServ.serverUrl = this.pluginSetting?.entryHost;
+    this.getCustomers(1, 10);
+  }
+
+  getCustomers(page: number, limit: number){
+    this.northwindServ.getCustomers(page, limit).subscribe({
+      next: res=>{
+        console.log(res);
+        this.dataList = res;
+      }
+    })
+  }
 }
