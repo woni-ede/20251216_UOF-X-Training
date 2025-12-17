8c8d2030a373ebd4b8f565f7048b68c302b21a47
woni <woni@edetw.com>
2025-12-17 13:53:08 +0800

    feat: 外掛欄位基本設定

--- Ede.Uofx.Customize.Web/ClientApp/src/app/web/inspect-field/write/inspect-field.write.component.ts
+++ Ede.Uofx.Customize.Web/ClientApp/src/app/web/inspect-field/write/inspect-field.write.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { BpmFwWriteComponent, UofxValidators } from '@uofx/web-components/form';
+import { BpmFwWriteComponent, UofxFormFieldLogic, UofxFormTools, UofxValidators } from '@uofx/web-components/form';
 
 @Component({
   selector: 'app-inspect-field.write',
@@ -18,12 +18,16 @@ export class InspectFieldWriteComponent extends BpmFwWriteComponent implements O
     { name: '需複驗', code: 'RETEST' }
   ];
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+    private tools: UofxFormTools,
+    private fieldLogic: UofxFormFieldLogic
+  ) {
     super();
   }
 
   ngOnInit() {
     this.initForm();
+    this.fieldUtils.syncParentFormStatusToInnerForm(this.form);
   }
 
   initForm() {
@@ -32,5 +36,24 @@ export class InspectFieldWriteComponent extends BpmFwWriteComponent implements O
       inspQuantity: [0, [Validators.required, Validators.min(1)]],
       inspResult: [null]
     })
+    this.setFormValue();
+  }
+
+  setFormValue() {
+    if (this.value) {
+      this.form.controls.comment.setValue(this.value.comment);
+      this.form.controls.inspQuantity.setValue(this.value.inspQuantity);
+      this.form.controls.inspResult.setValue(this.value.inspResult);
+    }
+  }
+
+  checkBeforeSubmit(checkValidator: boolean): Promise<boolean> {
+    return new Promise<boolean>(resolve => {
+      this.valueChanges.emit(this.form.value);
+      this.tools.markFormGroup(this.form);
+      this.fieldLogic.checkValidators(checkValidator, this.selfControl, this.form);
+      if (!checkValidator) return resolve(true);
+      resolve(this.form.valid);
+    });
   }
 }
