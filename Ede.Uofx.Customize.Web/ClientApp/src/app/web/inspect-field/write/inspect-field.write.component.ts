b2ada6dd47f1696ef2bb2f77dfcbb08098cd5cb3
woni <woni@edetw.com>
2025-12-17 13:33:06 +0800

    feat: 表單應用：必填屬性、驗證器、錯誤提示

--- Ede.Uofx.Customize.Web/ClientApp/src/app/web/inspect-field/write/inspect-field.write.component.ts
+++ Ede.Uofx.Customize.Web/ClientApp/src/app/web/inspect-field/write/inspect-field.write.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { BpmFwWriteComponent } from '@uofx/web-components/form';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BpmFwWriteComponent, UofxValidators } from '@uofx/web-components/form';
 
 @Component({
   selector: 'app-inspect-field.write',
   templateUrl: './inspect-field.write.component.html',
   styleUrl: './inspect-field.write.component.scss'
 })
-export class InspectFieldWriteComponent extends BpmFwWriteComponent {
+export class InspectFieldWriteComponent extends BpmFwWriteComponent implements OnInit {
 
+  form: FormGroup;
   errorMessage: string[] = [];
 
   inspResults = [
@@ -15,4 +17,20 @@ export class InspectFieldWriteComponent extends BpmFwWriteComponent {
     { name: '不通過', code: 'FAILED' },
     { name: '需複驗', code: 'RETEST' }
   ];
+
+  constructor(private fb: FormBuilder) {
+    super();
+  }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.form = this.fb.group({
+      comment: ['', [Validators.required, UofxValidators.notAllowedSpaceString]],
+      inspQuantity: [0, [Validators.required, Validators.min(1)]],
+      inspResult: [null]
+    })
+  }
 }
